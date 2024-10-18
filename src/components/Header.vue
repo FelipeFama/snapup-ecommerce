fd24b364a93da5118ad9e9d67cba17a5889ea308
felipe fama <lipehfama@gmail.com>
2024-10-18 00:20:32 -0300

    feat: add new styles and adjusts in header component and styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,76 +3,73 @@ import Navbar from "./Navbar.vue";
 </script>
 
 <template>
-  <header class="header text-white">
-    <nav class="container">
-      <section class="header-cnt">
-        <!-- Top Section with links and social media -->
-        <section
-          class="header-cnt-top fs-13 py-2 d-flex align-items-center justify-content-between"
-        >
-          <!-- Left Side: Seller Center and Social Links -->
-          <ul class="d-flex top-links align-items-center">
-            <li class="fs-6">
-              <router-link to="/seller">Seller Center</router-link>
-            </li>
-            <li class="vert-line" aria-hidden="true"></li>
-            <li class="fs-6">
-              <router-link to="/download">Download</router-link>
-            </li>
-            <li class="vert-line" aria-hidden="true"></li>
-            <li class="d-flex align-items-center">
-              <span class="fs-6">Follow us on</span>
-              <ul class="social-links d-flex align-items-center" aria-label="Social Media Links">
-                <li class="mx-2">
-                  <a href="https://www.facebook.com" class="fs-6" aria-label="Facebook">
-                    <i class="bi bi-facebook"></i>
-                  </a>
-                </li>
-                <li class="mx-2">
-                  <a href="https://www.instagram.com" class="fs-6" aria-label="Instagram">
-                    <i class="bi bi-instagram"></i>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-
-          <!-- Right Side: Support, Register, Log in -->
-          <ul class="top-links d-flex align-items-center">
-            <li>
-              <router-link to="/" class="top-link-itm">
-                <span class="top-link-itm-ico mx-2 fs-6">
-                  <i class="bi bi-question-circle-fill"></i>
-                </span>
-                <span class="top-link-itm-txt fs-6">Support</span>
-              </router-link>
-            </li>
-            <li class="vert-line" aria-hidden="true"></li>
-            <li>
-              <router-link to="/">
-                <span class="top-link-itm-txt fs-6">Register</span>
-              </router-link>
-            </li>
-            <li class="vert-line" aria-hidden="true"></li>
-            <li>
-              <router-link to="/">
-                <span class="top-link-itm-txt fs-6">Log in</span>
-              </router-link>
-            </li>
-          </ul>
+  <header className="header text-white">
+    <nav className="container">
+      <div className="header-cnt">
+        <section className="header-cnt-top fs-13 py-2 flex align-center justify-between">
+          <aside className="header-cnt-top-l">
+            <ul className="flex top-links align-center">
+              <li>
+                <router-link to="/seller">Seller Center</router-link>
+              </li>
+              <li className="vert-line"></li>
+              <li>
+                <router-link to="/download">Download</router-link>
+              </li>
+              <li className="vert-line"></li>
+              <li className="flex align-center">
+                <span className="fs-13">Follow us on</span>
+                <ul className="social-links flex align-center">
+                  <li className="mx-2">
+                    <a href="www.facebook.com" className="fs-15">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                  </li>
+                  <li className="mx-2">
+                    <a href="www.instagram.com" className="fs-15">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </aside>
+          <aside className="header-cnt-top-r">
+            <ul className="top-links flex align-center">
+              <li>
+                <router-link to="/" className="top-link-itm">
+                  <span className="top-link-itm-ico mx-2">
+                    <i className="bi bi-question-circle-fill"></i>
+                  </span>
+                  <span className="top-link-itm-txt">Support</span>
+                </router-link>
+              </li>
+              <li className="vert-line"></li>
+              <li>
+                <router-link to="/">
+                  <span className="top-link-itm-txt">Register</span>
+                </router-link>
+              </li>
+              <li className="vert-line"></li>
+              <li>
+                <router-link to="/">
+                  <span className="top-link-itm-txt">Log in</span>
+                </router-link>
+              </li>
+            </ul>
+          </aside>
         </section>
 
-        <!-- Bottom Section: Navbar -->
-        <section class="header-cnt-bottom">
+        <section className="header-cnt-bottom">
           <Navbar />
         </section>
-      </section>
+      </div>
     </nav>
   </header>
 </template>
 
 <style lang="scss" scoped>
-@import "../assets/styles/variables.scss";
+@import "../assets/styles/main.scss";
 
 .header {
   background: rgb(249, 78, 48);
