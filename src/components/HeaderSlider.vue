61a8d91c04ef9af01f6bd7d4ca81be44b69485cc
felipe fama <lipehfama@gmail.com>
2024-10-30 23:00:08 -0300

    feat: add bootstrap carousel in headerslider

--- src/components/HeaderSlider.vue
+++ src/components/HeaderSlider.vue
@@ -1,61 +1,28 @@
 <template>
-  <div class="slider">
-    <div class="container">
-      <div class="slider-content overflow-x-hidden">
-        <div
-          v-for="(img, index) in sliderImgs"
-          :key="index"
-          class="slider-item"
-          :style="{ transform: `translateX(-${currentSlide * 100}%)` }"
-        >
-          <img :src="img" alt="" />
-        </div>
-        <!--
-          <div class="dots">
-            <span 
-            v-for="(dot, index) in sliderImgs.length" 
-            :key="index" 
-            class="dot" 
-            :class="{ active: currentSlide === index }" 
-            @click="goToSlide(index)"
-            ></span>
-          </div>
-          -->
-      </div>
+  <article id="carouselExampleInterval" class="carousel slide slider" data-bs-ride="carousel">
+    <div class="carousel-inner container">
+      <!-- Loop through the imported images array -->
+      <figure
+        v-for="(img, index) in sliderImgs"
+        :key="index"
+        :class="['carousel-item slider-item', { active: index === 0 }]"
+        data-bs-interval="2000"
+      >
+        <img :src="img" class="d-block" alt="slider image" />
+      </figure>
     </div>
-  </div>
+  </article>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
-import { sliderImgs } from "@/utils/images"; // Adjust path as needed
+import { defineComponent } from "vue";
+import { sliderImgs } from "@/utils/images";
 
 export default defineComponent({
-  name: "HeaderSlider",
+  name: "BootstrapCarousel",
   setup() {
-    const currentSlide = ref(0);
-    const slideInterval = ref<ReturnType<typeof setInterval> | null>(null);
-
-    const goToSlide = (index: number) => {
-      currentSlide.value = index;
-    };
-
-    const startAutoplay = () => {
-      slideInterval.value = setInterval(() => {
-        currentSlide.value = (currentSlide.value + 1) % sliderImgs.length;
-      }, 3000);
-    };
-
-    onMounted(() => {
-      startAutoplay();
-    });
-
-    return {
-      currentSlide,
-      goToSlide,
-      sliderImgs
-    };
-  },
+    return { sliderImgs };
+  }
 });
 </script>
 
@@ -63,35 +30,7 @@ export default defineComponent({
 .slider {
   .slider-item {
     max-height: 300px;
-    display: inline-block;
     width: 100%;
   }
-
-  .slider-content {
-    display: flex;
-    overflow-y: hidden;
-    position: relative;
-    transition: transform 0.5s ease-in-out;
-  }
-   /*
-   .dots {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-    
-    .dot {
-      height: 10px;
-      width: 10px;
-      border-radius: 50%;
-      background-color: #ccc;
-      margin: 0 5px;
-      cursor: pointer;
-    }
-    
-    .dot.active {
-      background-color: #333;
-    }
-  }
-  */
 }
 </style>
