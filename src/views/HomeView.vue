b81648a1b5f7b0e4c18c2671026078f915ab63f3
felipe fama <lipehfama@gmail.com>
2024-11-27 23:29:56 -0300

    feat: add randomizing products list feature

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@
             </div>
             <!-- Conditionally render Loader or ProductList based on productStatus -->
             <Loader v-if="productStatus === STATUS.LOADING" />
-            <ProductList v-else :products="products" />
+            <ProductList v-else :products="tempProducts" />
           </div>
         </div>
       </div>
@@ -25,19 +25,36 @@ import HeaderSlider from "@/components/HeaderSlider.vue";
 import Loader from "@/components/Loader.vue";
 import ProductList from "@/components/ProductList.vue";
 import { useProductStore } from "@/stores/productStore";
+import type { IProducts } from "@/types/IProducts";
 import { STATUS } from "@/utils/status";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 
 const productStore = useProductStore();
-
-onMounted(async () => {
-  productStore.fetchProducts(50);
-});
 
 const products = computed(() => productStore.products);
 const productStatus = computed(() => productStore.productsStatus);
 
+//Randomizing the products in the list
+const tempProducts = ref<IProducts[]>([]);
+//console.log(tempProducts)
+watch(products, (newProducts) => {
+  if (newProducts.length > 0) {
+    const randomizedProducts: IProducts[] = [];
+    for (let i = 0; i < newProducts.length; i++) {
+      let randomIndex = Math.floor(Math.random() * newProducts.length);
 
+      while (randomizedProducts.includes(newProducts[randomIndex])) {
+        randomIndex = Math.floor(Math.random() * newProducts.length);
+      }
+      randomizedProducts.push(newProducts[randomIndex]);
+    }
+    tempProducts.value = randomizedProducts;
+  }
+});
+
+onMounted(async () => {
+  productStore.fetchProducts(50);
+});
 </script>
 
 <style lang="scss" scoped>
