037919c442b29d73fe2eb6b0f7dcd0de1f787b2b
felipe fama <lipehfama@gmail.com>
2024-11-29 11:54:20 -0300

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <Header />
   <Sidebar />
   <router-view />
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer bg-orange">
+    <div class="container py-4 text-center">
+      <div class="flex align-center justify-center text-white fw-3 fs-14">
+        <RouterLink to="/" class="text-uppercase">privacy policy</RouterLink>
+        <div class="vert-line"></div>
+        <RouterLink to="/" class="text-uppercase">term of service</RouterLink>
+        <div class="vert-line"></div>
+        <RouterLink to="/" class="text-uppercase">About SnapUp.</RouterLink>
+      </div>
+      <span class="text-white copyright-text text-manrope fs-14 fw-3">
+        &copy; 2022 SnapUp. All Rights Reserved.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/main.scss";
+
+.footer {
+  .copyright-text {
+    display: block;
+    margin-top: 0.7rem;
+  }
+}
+</style>
