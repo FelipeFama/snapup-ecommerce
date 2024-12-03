09966ee0416eb5cfb6796517ea02b102c6964226
felipe fama <lipehfama@gmail.com>
2024-12-03 11:28:22 -0300

    feat: add init logic route in productsingle page

--- src/views/ProductSingle.vue
+++ src/views/ProductSingle.vue
@@ -1,7 +1,24 @@
 <template>
   <main class="about">
-    <h1>This is an about page</h1>
+    <h1>This an product page</h1>
   </main>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import { useProductStore } from "@/stores/productStore";
+import { STATUS } from "@/utils/status";
+import { formatPrice } from "@/utils/helpers";
+import Loader from "@/components/Loader.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const id = route.params.id as string;
+console.log(id)
+
+const productStore = useProductStore();
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/main.scss";
+</style>
