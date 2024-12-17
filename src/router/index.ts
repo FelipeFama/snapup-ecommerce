08330f196cc688ff62dcf2c2eda47e0a72e19726
felipe fama <lipehfama@gmail.com>
2024-12-17 10:45:30 -0300

    update project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import ProductSingle from "@/views/ProductSingle.vue";
-import CategoryProduct from "@/views/CategoryProduct.vue";
-import Cart from "@/views/Cart.vue";
-import Search from "@/views/Search.vue";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,25 +14,25 @@ export const router = createRouter({
     {
       path: "/product/:id",
       name: "product",
-      component: ProductSingle
+      component: () => import("@/views/ProductSingle.vue")
     },
     // category wise product listing route
     {
       path: "/category/:category",
       name: "category",
-      component: CategoryProduct
+      component: () => import("@/views/CategoryProduct.vue")
     },
     // cart
     {
       path: "/cart",
       name: "cart",
-      component: Cart
+      component: () => import("@/views/Cart.vue")
     },
     // searched products
     {
       path: "/search/:searchTerm",
       name: "search",
-      component: Search
+      component: () => import("@/views/Search.vue")
     }
   ]
 });
