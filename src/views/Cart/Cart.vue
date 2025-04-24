fda3770142724b33a3ddcb3356006c6af2dec16d
Felipe Fama <lipehfama.dev@gmail.com>
2025-04-24 20:45:47 -0300

    feat: add imports in cart page

--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -4,6 +4,16 @@
   </main>
 </template>
 
+<script setup lang="ts">
+import { useCartStore } from "@/stores/cartStore";
+import { formatPrice } from "@/utils/helpers";
+import { shopping_cart } from "@/utils/images";
+import { computed } from "vue";
+
+const cartStore = computed(() => useCartStore());
+//console.log(cartStore.value)
+</script>
+
 <style lang="scss" scoped>
 @import "./Cart.module.scss";
 </style>
