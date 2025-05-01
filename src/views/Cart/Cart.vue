81f14292913f3119431efe5adfdb45ff12905926
Felipe Fama <lipehfama.dev@gmail.com>
2025-04-30 21:19:54 -0300

    feat: add render dates in table and update styles

--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -1,9 +1,9 @@
 <template>
-  <main v-if="carts.length === 0" class="container my-5">
-    <article class="empty-cart flex justify-center align-center flex-column font-manrope">
+  <main v-if="carts.length === 0" class="container mt-5">
+    <article class="empty-cart d-flex justify-content-center align-items-center flex-column font-manrope">
       <img :src="shopping_cart" alt="shopping cart" />
-      <span class="fw-6 fs-15 text-gray">Your shopping cart is empty.</span>
-      <RouterLink to="/" class="shopping-btn bg-orange text-white fw-5">Go shopping Now</RouterLink>
+      <span class="fw-semibold fs-5 text-gray">Your shopping cart is empty.</span>
+      <RouterLink to="/" class="shopping-btn bg-orange text-white fw-medium">Go shopping Now</RouterLink>
     </article>
   </main>
   <main v-else class="cart bg-whitesmoke">
@@ -31,6 +31,54 @@
             </aside>
           </article>
         </section>
+        <section class="cart-cbodhy bg-white">
+          <article class="cart-ctr py-4" v-for="(cart, idx) in carts" :key="cart.id">
+            <aside class="cart-ctd">
+              <span class="cart-ctxt">{{ idx + 1 }}</span>
+            </aside>
+            <aside class="cart-ctd">
+              <span class="cart-ctxt">{{ cart.title }}</span>
+            </aside>
+            <aside class="cart-ctd">
+              <span class="cart-ctxt">{{ formatPrice(cart.discountedPrice) }}</span>
+            </aside>
+            <aside class="cart-ctd">
+              <div class="qty-change d-flex align-items-center">
+                <button
+                  type="button"
+                  class="qty-decrease d-flex align-items-center justify-content-center"
+                  @click="cartStore.toggleCartQty({ id: cart.id, type: 'DEC' })"
+                >
+                  <i class="bi bi-dash"></i>
+                </button>
+                <div class="qty-value d-flex align-items-center justify-content-center">
+                  {{ cart.quantity }}
+                </div>
+                <button
+                  type="button"
+                  class="qty-increase d-flex align-items-center justify-content-center"
+                  @click="cartStore.toggleCartQty({ id: cart.id, type: 'INC' })"
+                >
+                  <i class="bi bi-plus"></i>
+                </button>
+              </div>
+            </aside>
+            <aside class="cart-ctd">
+              <span class="cart-ctxt text-orange fw-medium">
+                {{ formatPrice(cart.totalPrice) }}
+              </span>
+            </aside>
+            <aside class="cart-ctd">
+              <button
+                type="button"
+                class="delete-btn text-dark"
+                @click="cartStore.removeFromCart(cart.id)"
+              >
+                Delete
+              </button>
+            </aside>
+          </article>
+        </section>
       </section>
     </div>
   </main>
@@ -41,12 +89,17 @@ import { useCartStore } from "@/stores/cartStore";
 import { formatPrice } from "@/utils/helpers";
 import { shopping_cart } from "@/utils/images";
 import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-//const cartStore = computed(() => useCartStore());
+const router = useRouter();
 const cartStore = useCartStore();
-const { carts,itemsCount,totalAmount } = storeToRefs(cartStore);
+//const cartStore = computed(() => useCartStore());
+const { carts, itemsCount, totalAmount } = storeToRefs(cartStore);
 //console.log(itemsCount.value, totalAmount.value);
+onMounted(() => {
+  cartStore.getCartTotal(carts.value);
+});
 </script>
 
 <style lang="scss" scoped>
