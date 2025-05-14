a3f0406e96409a7ae035fd9ea1d8683409669f69
Felipe Fama <lipehfama.dev@gmail.com>
2025-05-13 23:34:32 -0300

    update setting in project

--- src/views/CategoryProduct/CategoryProduct.vue
+++ src/views/CategoryProduct/CategoryProduct.vue
@@ -1,10 +1,16 @@
 <template>
   <main class="cat-products py-5 bg-whitesmoke">
-    <section class="container">
-      test
-    </section>
+    <section class="container">test</section>
   </main>
 </template>
+
+<script setup lang="ts">
+import { useCategoryStore } from "@/stores/categoryStore";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const categoryStore = useCategoryStore();
+</script>
 
 <style lang="scss" scoped>
 @import "./CategoryProduct.module.scss";
